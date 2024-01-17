895c9e7956fde3749fecefeb651cd6a709e446bf
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-01-17 11:49:25 +0200

    Solution to the task

--- Stacks, Queues, Tuples and Sets - Exercise/05.santas_present_factory.py
+++ Stacks, Queues, Tuples and Sets - Exercise/05.santas_present_factory.py
@@ -15,30 +15,45 @@ while materials and magic:
 
     magic_level = curr_material * curr_magic
 
-    if magic_level < 0:
+    if magic_level in presents.values():
+        for key, value in presents.items():
+            if value == magic_level:
+                if key in crafted_toys.keys():
+                    crafted_toys[key] += 1
+                    continue
+                crafted_toys[key] = 1
+    elif magic_level < 0:
         materials.append(curr_material + curr_magic)
     elif magic_level > 0:
         curr_material += 15
         materials.append(curr_material)
-    elif magic_level in presents.values():
-        for key, value in presents:
-            if value == magic_level:
-                crafted_toys[key] += 1
+    else:
+        if curr_material == 0 and curr_magic == 0:
+            continue
+        elif curr_material == 0:
+            magic.appendleft(curr_magic)
+        elif curr_magic == 0:
+            materials.append(curr_material)
 
     if ("Doll" in crafted_toys.keys() and
         "Wooden train" in crafted_toys.keys()) or (
             "Teddy bear" in crafted_toys.keys() and
             "Bicycle" in crafted_toys.keys()):
         success = True
-        break
 
 if success:
     print("The presents are crafted! Merry Christmas!")
 else:
     print("No presents this Christmas!")
+
 if materials:
-    print("Materials left:", ", ".join(map(str, materials)))
+    start = materials.pop()
+    end = materials.popleft()
+    full = start, *materials, end
+    print("Materials left:", ", ".join(map(str, full)))
+
 if magic:
     print("Magic left:", ", ".join(map(str, magic)))
-for toy, num in crafted_toys.items():
+
+for toy, num in sorted(crafted_toys.items()):
     print(f"{toy}: {num}")
