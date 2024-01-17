322b9d2e249680ba922c14a326a764d0d27e89da
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-01-17 10:31:59 +0200

    Solution to the task

---
+++ Stacks, Queues, Tuples and Sets - Exercise/05.santas_present_factory.py
@@ -0,0 +1,8 @@
+from collections import deque
+
+materials = deque(input().split())
+magic = deque(input().split())
+
+presents = {"Doll": 150, "Wooden train": 250,
+            "Teddy bear": 300, "Bicycle": 400}
+
