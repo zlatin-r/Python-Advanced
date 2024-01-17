63c8a52e05fd86d5f01e555dd9a1916b37e61ade
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-01-17 10:57:44 +0200

    Solution to the task

--- Stacks, Queues, Tuples and Sets - Exercise/05.santas_present_factory.py
+++ Stacks, Queues, Tuples and Sets - Exercise/05.santas_present_factory.py
@@ -1,8 +1,44 @@
 from collections import deque
 
-materials = deque(input().split())
-magic = deque(input().split())
+materials = deque(int(x) for x in input().split())
+magic = deque(int(x) for x in input().split())
 
 presents = {"Doll": 150, "Wooden train": 250,
             "Teddy bear": 300, "Bicycle": 400}
 
+crafted_toys = {}
+success = False
+
+while materials and magic:
+    curr_material = materials.pop()
+    curr_magic = magic.popleft()
+
+    magic_level = curr_material * curr_magic
+
+    if magic_level < 0:
+        materials.append(curr_material + curr_magic)
+    elif magic_level > 0:
+        curr_material += 15
+        materials.append(curr_material)
+    elif magic_level in presents.values():
+        for key, value in presents:
+            if value == magic_level:
+                crafted_toys[key] += 1
+
+    if ("Doll" in crafted_toys.keys() and
+        "Wooden train" in crafted_toys.keys()) or (
+            "Teddy bear" in crafted_toys.keys() and
+            "Bicycle" in crafted_toys.keys()):
+        success = True
+        break
+
+if success:
+    print("The presents are crafted! Merry Christmas!")
+else:
+    print("No presents this Christmas!")
+if materials:
+    print("Materials left:", ", ".join(map(str, materials)))
+if magic:
+    print("Magic left:", ", ".join(map(str, magic)))
+for toy, num in crafted_toys.items():
+    print(f"{toy}: {num}")
