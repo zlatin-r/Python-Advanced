7a5f7c4212c634c042f83c37d8d17fd6031e2bd4
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-01-19 15:20:32 +0200

    Solution to the tasks

---
+++ Multidimensional Lists - Exercise 1/05.matrix_of_palindromes.py
@@ -0,0 +1,15 @@
+rows, columns = [int(x) for x in input().split()]
+
+matrix = []
+
+for row_idx in range(rows):
+    row = []
+
+    for col_idx in range(row_idx, columns):
+        row.append(chr(row_idx + 97) + chr(col_idx + 97) + chr(row_idx + 97))
+
+    matrix.append(row)
+    columns += 1
+
+for i in matrix:
+    print(*i)
