f2fa950b06216b54e8c304cea1aea507c80ef6d8
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-01-19 18:15:35 +0200

    Solution to the tasks

--- Multidimensional Lists - Exercise 1/01.diagonals.py
+++ Multidimensional Lists - Exercise 1/01.diagonals.py
@@ -6,13 +6,16 @@ secondary_diagonal = []
 
 for _ in range(n):
     matrix.append([int(x) for x in input().split(", ")])
+    #  matrix = [[int(x) for x in input().split(", ")] for _ in range(n)]
 
 for i in range(len(matrix)):
     primary_diagonal.append(matrix[i][i])
+    #  primary_diagonal = [matrix[i][i] for i in range(n)]
 
 for j in range(len(matrix)):
     secondary_diagonal.append(matrix[j][n - 1])
     n -= 1
+    #  secondary_diagonal = [matrix[j][n - j - 1] for j in range(n)]
 
 print(f"Primary diagonal: {', '.join(str(x) for x in primary_diagonal)}. Sum: {sum(primary_diagonal)}")
 print(f"Secondary diagonal: {', '.join(str(x) for x in secondary_diagonal)}. Sum: {sum(secondary_diagonal)}")
