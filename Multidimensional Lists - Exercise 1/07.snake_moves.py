913d4059f2721d19d435fa9d79fe74c8c6f34f6f
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-01-19 19:20:55 +0200

    Solution to the tasks

--- Multidimensional Lists - Exercise 1/07.snake_moves.py
+++ Multidimensional Lists - Exercise 1/07.snake_moves.py
@@ -3,10 +3,7 @@ from collections import deque
 rows, columns = [int(x) for x in input().split()]
 snake = deque(x for x in input())
 
-matrix = []
-
-for _ in range(rows):
-    matrix.append(['*' for _ in range(columns)])
+matrix = [['*' for _ in range(columns)] for _ in range(rows)]
 
 for i in range(rows):
     for j in range(columns):
@@ -18,5 +15,4 @@ for i in range(rows):
     if i % 2 == 1:
         matrix[i].reverse()
 
-for row in matrix:
-    print("".join(row))
+[print("".join(row)) for row in matrix]
