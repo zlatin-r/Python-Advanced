0739b8d162a2b69c1ae791f7e23399b907ab81e5
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-01-23 14:02:42 +0200

    Solution to the task

--- Multidimensional Lists - Exercise 2/06.range_day.py
+++ Multidimensional Lists - Exercise 2/06.range_day.py
@@ -2,7 +2,7 @@ size = 5
 matrix = []
 current_pos = [0, 0]
 total_targets = 0
-targets_hit = 0
+hits = 0
 hit_positions = []
 
 directions = {
@@ -21,11 +21,9 @@ for row in range(size):
     if "x" in matrix[row]:
         total_targets += matrix[row].count("x")
 
-n = int(input())
-for _ in range(n):
+for _ in range(int(input())):
     command = input().split()
-    action = command[0]
-    direction = command[1]
+    action, direction = command[0], command[1]
 
     if action == "move":
         steps = int(command[2])
@@ -42,18 +40,18 @@ for _ in range(n):
         while 0 <= r < size and 0 <= c < size:
             if matrix[r][c] == "x":
                 matrix[r][c] = "."
-                targets_hit += 1
+                hits += 1
                 hit_positions.append([r, c])
                 break
 
             r += directions[direction][0]
             c += directions[direction][1]
 
-        if targets_hit == total_targets:
+        if hits == total_targets:
             break
 
-if total_targets != targets_hit:
-    print(f"Training not completed! {total_targets - targets_hit} targets left.")
+if total_targets != hits:
+    print(f"Training not completed! {total_targets - hits} targets left.")
 else:
     print(f"Training completed! All {total_targets} targets hit.")
 [print(x) for x in hit_positions]
