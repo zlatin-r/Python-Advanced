2415b666c5be0f3b9997a83297632830449dba68
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-01-25 13:45:44 +0200

    Solution to the task

---
+++ Functions Advanced - Lab/03.cheese_showcase.py
@@ -0,0 +1,19 @@
+def sorting_cheeses(**cheeses_dict):
+    cheeses_dict = sorted(cheeses_dict.items(),key=lambda x: (-len(x[1]), x[0]))
+    result = []
+
+    for (cheese_name, quantities) in cheeses_dict:
+        result.append(cheese_name)
+        quantity_list = sorted(quantities, reverse=True)
+        result += quantity_list
+
+    return "\n".join([str(x) for x in result])
+
+
+print(
+    sorting_cheeses(
+        Parmesan=[102, 120, 135],
+        Camembert=[100, 100, 105, 500, 430],
+        Mozzarella=[50, 125],
+    )
+)
