4489662bb6a9381e53e34af868968cbd74948236
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-01-25 14:44:59 +0200

    Solution to the task

--- Functions Advanced - Exercise/01.negative_vs_positive.py
+++ Functions Advanced - Exercise/01.negative_vs_positive.py
@@ -1,3 +1,29 @@
-def negative_vs_positive(*args):
+numbers = [int(x) for x in input().split()]
 
-    pass
+
+def negative_sum(*args):
+    neg_sum = 0
+    for arg in args[0]:
+        if arg < 0:
+            neg_sum += arg
+    return neg_sum
+
+
+def positive_sum(*args):
+    pos_sum = 0
+    for arg in args[0]:
+        if arg > 0:
+            pos_sum += arg
+    return pos_sum
+
+
+def compare(a, b):
+    if abs(a) > abs(b):
+        return "The negatives are stronger than the positives"
+    elif abs(a) < abs(b):
+        return "The positives are stronger than the negatives"
+
+
+print(negative_sum(numbers))
+print(positive_sum(numbers))
+print(compare(negative_sum(numbers), positive_sum(numbers)))
