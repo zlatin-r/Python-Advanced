09a17ce81747be99762db43fcc94234e8aaa6f56
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-01-25 18:41:09 +0200

    Solution to the task

--- Functions Advanced - Exercise/03.even_or_odd.py
+++ Functions Advanced - Exercise/03.even_or_odd.py
@@ -1,12 +1,9 @@
 def even_odd(*args):
-    result = None
 
     if args[-1] == "even":
-        result = [int(x) for x in args[:-1] if x % 2 == 0]
+        return [int(x) for x in args[:-1] if x % 2 == 0]
     else:
-        result = [int(x) for x in args[:-1] if x % 2 == 1]
-
-    return result
+        return [int(x) for x in args[:-1] if x % 2 == 1]
 
 
 print(even_odd(1, 2, 3, 4, 5, 6, "even"))
