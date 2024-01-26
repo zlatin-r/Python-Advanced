5ac4fab49572ceeb85d932c7d205ce51b4f1a3d2
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-01-26 14:11:22 +0200

    Solution to the task

--- Functions Advanced - Exercise/11.math_operations.py
+++ Functions Advanced - Exercise/11.math_operations.py
@@ -1,2 +1,14 @@
-def math_operations():
-    pass
+from collections import deque
+
+operators = ["+", "-", "/", "*"]
+
+
+def math_operations(*args, **kwargs):
+
+    numbers = deque(args)
+
+    while numbers:
+
+        for k, v in kwargs.items():
+            current_number = numbers.popleft()
+            kwargs[k] = current_number + v
