f5505f692d902a0fa376b848dfd0e2467bb06819
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-01-30 07:35:09 +0200

    Solution to the task

--- Error Handling/01.numbers_dictionary.py
+++ Error Handling/01.numbers_dictionary.py
@@ -5,10 +5,10 @@ line = input()
 while line != "Search":
     number_as_string = line
 
-    try:                                                  # try block to test for ValueError added
+    try:                                                  # try block added
         number = int(input())
         numbers_dictionary[number_as_string] = number
-    except ValueError:                                    # except block added to handles the error if occurs
+    except ValueError:                                    # except block added to handle ValueError if occurs
         print("The variable number must be an integer")
 
     line = input()                                        # second input in to the while loop added
@@ -27,9 +27,9 @@ line = input()
 while line != "End":
     searched = line
 
-    try:                                                  # try block to test for KeyError added
+    try:                                                  # try block added
         del numbers_dictionary[searched]
-    except KeyError:                                      # except block added to handle the error if occurs
+    except KeyError:                                      # except block added to handle KeyError if occurs
         print("Number does not exist in dictionary")
 
     line = input()                                        # second input in to the while loop added
