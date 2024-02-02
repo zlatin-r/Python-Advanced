bbe1dc0d944633580e08b603c8918c82436841b7
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-02-02 12:53:29 +0200

    Solution to the task

--- Python Advanced: Exam Preparation/8.01.energy_drinks.py
+++ Python Advanced: Exam Preparation/8.01.energy_drinks.py
@@ -3,29 +3,26 @@ from collections import deque
 caffeine = [int(x) for x in input().split(", ")]
 drinks = deque(int(x) for x in input().split(", "))
 
-max_caffeine = 300
-total_caffeine = 0
+stamats_caffeine = 0
 
 while drinks and caffeine:
     curr_caffeine = caffeine.pop()
     curr_drink = drinks.popleft()
 
-    result = curr_drink * curr_caffeine
+    caffeine = curr_drink * curr_caffeine
 
-    if result <= max_caffeine:
-        max_caffeine -= result
-        total_caffeine += result
-    elif result > max_caffeine:
+    if caffeine + stamats_caffeine <= 300:
+        stamats_caffeine += caffeine
+    else:
         drinks.append(curr_drink)
-        total_caffeine -= 30
-        max_caffeine += 30
+        stamats_caffeine -= 30
 
-        if max_caffeine < 0:
-            max_caffeine = 0
+        if stamats_caffeine < 0:
+            stamats_caffeine = 0
 
 if drinks:
     print(f"Drinks left: {', '.join(map(str, drinks))}")
 else:
     print(f"At least Stamat wasn't exceeding the maximum caffeine.")
 
-print(f"Stamat is going to sleep with {total_caffeine} mg caffeine.")
+print(f"Stamat is going to sleep with {stamats_caffeine} mg caffeine.")
