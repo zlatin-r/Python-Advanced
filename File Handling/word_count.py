ddf5c5659fd5e83d7e74a49ad67fdf9d0b04eee5
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-02-04 12:06:13 +0200

    Solution to the task

--- File Handling/word_count.py
+++ File Handling/word_count.py
@@ -1,2 +1,23 @@
-with open('words.txt', 'w') as f:
-    f.write("quick is fault")
+import re
+
+words_count = {}
+
+with open("resources/words.txt") as file:
+    searched_words_as_text = file.read()
+    searched_words = [word.lower() for word in searched_words_as_text.split()]
+
+with open("resources/input.txt") as file:
+    content = file.read().lower()
+
+
+for word in searched_words:
+    pattern = re.compile(rf"\b{word}\b")
+    result = re.findall(pattern, content)
+
+    words_count[word] = len(result)
+
+sorted_words_count = sorted(words_count.items(), key=lambda x: -x[1])
+
+with open("resources/output.txt", "a") as file:
+    for word, count in sorted_words_count:
+        file.write(f"{word} - {count}\n")
