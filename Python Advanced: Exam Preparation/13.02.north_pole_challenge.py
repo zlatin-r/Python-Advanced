1f9edab7c9b2061509eb12a05755f1332ad38e37
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-02-09 15:52:37 +0200

    Solution to the task

--- Python Advanced: Exam Preparation/13.02.north_pole_challenge.py
+++ Python Advanced: Exam Preparation/13.02.north_pole_challenge.py
@@ -1,11 +1,66 @@
+def row_col_pos_checker(r, c):
+    if r > rows - 1:
+        r = 0
+    if r < 0:
+        r = rows -1
+    if c > columns - 1:
+        c = 0
+    if c < 0:
+        c = columns - 1
+
+    return r, c
+
+
 rows, columns = map(int, input().split())
 
-collected_items = []
 field = []
+total_items = 0
+collected_items = {"D": 0, "G": 0, "C": 0}
+
+directions = {
+    "up": (-1, 0),
+    "down": (1, 0),
+    "left": (0, -1),
+    "right": (0, 1),
+}
 
 for row in range(rows):
     field.append(input().split())
 
     if "Y" in field[row]:
-        start_row, start_col = row, field[row].index("Y")
+        row_pos, col_pos = row, field[row].index("Y")
+
+    total_items += field[row].count("D")
+    total_items += field[row].count("G")
+    total_items += field[row].count("C")
+
+command = input()
+
+while command != "End":
+    direction, steps = command.split("-")
+
+    for step in range(int(steps)):
+
+        row_pos += directions[direction][0]
+        col_pos += directions[direction][1]
+
+        row_col_pos_checker(row_pos, col_pos)
+
+        current_position = field[row_pos][col_pos]
+
+        if current_position == "D":
+            collected_items["D"] += 1
+        elif current_position == "G":
+            collected_items["G"] += 1
+        elif current_position == "C":
+            collected_items["C"] += 1
+
+        if total_items == sum(collected_items.values()):
+            print("Merry Christmas!")
+            break
+print("You've collected:")
+print(f"- {collected_items['D']} Christmas decorations")
+print(f"- {collected_items['G']} Gifts")
+print(f"- {collected_items['C']} Cookies")
+
 
