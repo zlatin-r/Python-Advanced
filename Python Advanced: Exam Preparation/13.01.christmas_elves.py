4771164bf5651d43926e6d54b5583035613884cb
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-02-09 15:24:59 +0200

    Solution to the task

--- Python Advanced: Exam Preparation/13.01.christmas_elves.py
+++ Python Advanced: Exam Preparation/13.01.christmas_elves.py
@@ -9,37 +9,46 @@ total_energy = 0
 
 while elves_energy and material_boxes:
 
-    current_evl_energy = elves_energy.popleft()
-    current_material_box = material_boxes.pop()
+    elf_energy = elves_energy.popleft()
+    material_box = material_boxes.pop()
+
+    if elf_energy < 5:
+        material_boxes.append(material_box)
+        continue
 
     box += 1
 
     if box % 3 == 0:
-        current_material_box *= 2
+        material_box *= 2
 
-        if current_material_box > current_evl_energy:
-            current_material_box /= 2
-
-    if current_evl_energy >= current_material_box:
-        current_evl_energy -= current_material_box
-        total_energy += current_material_box
+    if elf_energy >= material_box:
+        elf_energy -= material_box
+        total_energy += material_box
 
         if box % 5 == 0:
-            elves_energy.append(current_evl_energy)
+            elves_energy.append(elf_energy)
             continue
 
-        current_evl_energy += 1
-        elves_energy.append(current_evl_energy)
+        elf_energy += 1
+        elves_energy.append(elf_energy)
 
         if box % 3 == 0:
             total_toys += 2
         else:
             total_toys += 1
     else:
-        material_boxes.append(current_material_box)
-        current_evl_energy *= 2
-        elves_energy.append(current_evl_energy)
+        if box % 3 == 0:
+            material_box /= 2
+        material_boxes.append(int(material_box))
+
+        elf_energy *= 2
+        elves_energy.append(elf_energy)
 
 print(f"Toys: {total_toys}")
 print(f"Energy: {int(total_energy)}")
-print(f"Elves left: {', '.join(str(x) for x in elves_energy)}")
+
+if elves_energy:
+    print(f"Elves left: {', '.join(str(x) for x in elves_energy)}")
+
+if material_boxes:
+    print(f"Boxes left: {', '.join(str(x) for x in material_boxes)}")
