79c9eeb2f8a6773f2e6f6f84586eb6e9f8296519
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-02-10 11:54:38 +0200

    Solution to the task

--- Python Advanced: Exam Preparation/14.02.ball_in_the_bucket.py
+++ Python Advanced: Exam Preparation/14.02.ball_in_the_bucket.py
@@ -8,13 +8,14 @@ for _ in range(SIZE):
     board.append(input().split())
 
 for throw in range(3):
-    coordinates = input().strip("(").strip(")").split(",")
-    r = int(coordinates[0])
-    c = int(coordinates[1])
+
+    r, c = map(int, input().strip("(").strip(")").split(","))
 
     if 0 <= r < SIZE and 0 <= c < SIZE:
+
         if board[r][c] == "B":
             board[r][c] = 0
+
             for i in range(SIZE):
                 scored_points += int(board[i][c])
 
