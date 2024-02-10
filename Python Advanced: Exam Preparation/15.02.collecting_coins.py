865fcab21e77282580c238404c5c4e37f1c135bc
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-02-10 14:16:14 +0200

    Solution to the task

--- Python Advanced: Exam Preparation/15.02.collecting_coins.py
+++ Python Advanced: Exam Preparation/15.02.collecting_coins.py
@@ -14,7 +14,7 @@ def row_col_pos_checker(r, c):
 SIZE = int(input())
 
 field = []
-r, c = 0, 0
+row, col = 0, 0
 path = []
 collected_coins = 0
 
@@ -25,21 +25,35 @@ directions = {
     "right": (0, 1),
 }
 
-for row in range(SIZE):
+for rows in range(SIZE):
     field.append(input().split())
 
-    if "X" in field[row]:
-        r, c = row, field[row].index("P")
+    if "P" in field[rows]:
+        row, col = rows, field[rows].index("P")
+        path.append([row, col])
 
 while True:
     command = input()
 
-    r += directions[command][0]
-    c += directions[command][1]
+    row += directions[command][0]
+    col += directions[command][1]
 
-    r, c = row_col_pos_checker(r, c)
+    row, col = row_col_pos_checker(row, col)
+    path.append([row, col])
 
-    position = field[r][c]
+    position = field[row][col]
 
-    if position == ""
+    if position.isdigit():
+        collected_coins += int(position)
+        field[row][col] = "0"
+    elif position == "X":
+        collected_coins /= 2
+        print(f"Game over! You've collected {int(collected_coins // 1)} coins.")
+        break
 
+    if collected_coins >= 100:
+        print(f"You won! You've collected {collected_coins} coins.")
+        break
+
+print("Your path:")
+[print(item) for item in path]
