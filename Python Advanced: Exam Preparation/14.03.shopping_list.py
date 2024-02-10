ff9a38b5d458635b7a9d5faf302ac1dbfe63375d
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-02-10 12:19:23 +0200

    Solution to the task

--- Python Advanced: Exam Preparation/14.03.shopping_list.py
+++ Python Advanced: Exam Preparation/14.03.shopping_list.py
@@ -9,7 +9,7 @@ def shopping_list(budget: int, **kwargs):
 
             if total_price <= budget:
                 budget -= total_price
-                print(f"You bought {key} for {total_price:.2f} leva.")
+                result += f"You bought {key} for {total_price:.2f} leva.\n"
                 basket += 1
 
             if basket == 5:
@@ -17,11 +17,26 @@ def shopping_list(budget: int, **kwargs):
     else:
         result += "You do not have enough budget."
 
-    return result
+    return result.strip()
 
 
 print(shopping_list(100,
                     microwave=(70, 2),
                     skirts=(15, 4),
                     coffee=(1.50, 10),
+                    ))
+
+print(shopping_list(20,
+                    jeans=(19.99, 1),
+                    ))
+
+print(shopping_list(104,
+                    cola=(1.20, 2),
+                    candies=(0.25, 15),
+                    bread=(1.80, 1),
+                    pie=(10.50, 5),
+                    tomatoes=(4.20, 1),
+                    milk=(2.50, 2),
+                    juice=(2, 3),
+                    eggs=(3, 1),
                     ))
