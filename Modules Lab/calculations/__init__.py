438eb18139e9ed843bc902f5b5378f535ca59fed
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-02-10 15:20:36 +0200

    Solution to the task

---
+++ Modules Lab/calculations/__init__.py
@@ -0,0 +1,18 @@
+def divide(num1, num2):
+    return num1 / num2
+
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+def subtract(num1, num2):
+    return num1 - num2
+
+
+def addition(num1, num2):
+    return num1 + num2
+
+
+def power(num1, num2):
+    return num1 ** num2
