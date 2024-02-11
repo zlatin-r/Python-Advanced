0f714759f1da26eb33132e755f9ac01ae85c558a
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-02-11 10:41:57 +0200

    Solution to the task

--- Python Advanced: Exam Preparation/16.02.darts.py
+++ Python Advanced: Exam Preparation/16.02.darts.py
@@ -13,12 +13,13 @@ for row in range(SIZE):
 
 while True:
     r, c = map(int, input().strip("(").strip(")").split(', '))
-    current_target = dartboard[r][c]
 
     player = players_names[0]
     turns[player] += 1
 
     if 0 <= r < SIZE and 0 <= c < SIZE:
+
+        current_target = dartboard[r][c]
         bonus_sum = int(dartboard[r][0]) + int(dartboard[r][6]) + int(dartboard[0][c]) + int(dartboard[6][c])
 
         if current_target.isdigit():
