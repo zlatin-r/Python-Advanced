c72fffe309b8602f8eaf68b9d21a84b36d2882a6
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-02-12 16:29:49 +0200

    Solution to the task

--- Python Advanced: Exam Preparation/18.02.checkmate.py
+++ Python Advanced: Exam Preparation/18.02.checkmate.py
@@ -1,25 +1,43 @@
 SIZE = 8
 board = []
 capture_coordinates = []
+king_pos = []
 
 directions = {
     "up": (-1, 0),
     "down": (1, 0),
     "left": (0, -1),
     "right": (0, 1),
+    "up_right": (-1, 1),
+    "up_left": (-1, -1),
+    "down_left": (1, -1),
+    "down_right": (1, 1)
 }
 
 for rows in range(SIZE):
     board.append(input().split())
 
-for r in range(SIZE):
-    for c in range(SIZE):
-        current_pos = board[r][c]
+    if "K" in board[rows]:
+        king_pos = [rows, board[rows].index("K")]
 
-        if current_pos == "Q":
-            for direction in directions:
-                while board[r][c] != "K" or 0 <= r < SIZE and 0 <= c < SIZE:
-                    r += direction[0]
-                    c += direction[1]
-                else:
-                    capture_coordinates.append([r, c])
+r, c = king_pos
+
+
+for direction in directions:
+    while True:
+        r += directions[direction][0]
+        c += directions[direction][1]
+
+        if 0 <= r < SIZE and 0 <= c < SIZE:
+            if board[r][c] == "Q":
+                capture_coordinates.append([r, c])
+                r, c = king_pos
+                break
+        else:
+            r, c = king_pos
+            break
+
+if capture_coordinates:
+    print(*capture_coordinates, sep="\n")
+else:
+    print("The king is safe!")
