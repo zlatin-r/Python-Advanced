060fb1404c80cd2ccc7afc0a9bcd61463117dc47
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-02-15 11:00:00 +0200

    Solution to the task

---
+++ Python Advanced: Exam Preparation/4.03.1.enrollment.py
@@ -0,0 +1,18 @@
+def gather_credits(needed_credits, *courses_info):
+    gathered_credits = 0
+    enrolled_courses = []
+
+    for course_name, course_credits in courses_info:
+        if gathered_credits >= needed_credits:
+            break
+        if course_name in enrolled_courses:
+            continue
+        enrolled_courses.append(course_name)
+        gathered_credits += course_credits
+
+    if gathered_credits >= needed_credits:
+        return f"""Enrollment finished! Maximum credits: {gathered_credits}.
+Courses: {', '.join(sorted(enrolled_courses))}"""
+    return f"You need to enroll in more courses! You have to gather {needed_credits - gathered_credits} credits more."
+
+# Optimized performance Time: 0.090 s
